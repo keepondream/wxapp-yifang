3607a77a38f30e717fc5ac60e2a563dea4688a85
keepondream <402314889@qq.com>
2018-09-05 10:02:18 +0800

    调整列表样式

--- pages/floors/floors.js
+++ pages/floors/floors.js
@@ -12,10 +12,10 @@ Page({
     isFoldParam: true,
     viewHightImg: false,
     viewHightBig: false,
-    viewHightBigStatus:0,
-      loadingHidden: true,
+    viewHightBigStatus: 0,
+    loadingHidden: true,
     overview: '',
-      landscape:'',
+    landscape: '',
     longitude: '',
     latitude: '',
     markers: [],
@@ -57,8 +57,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-      this.showNavigationBarLoading()
-      this.openLoading()
+    this.showNavigationBarLoading()
+    this.openLoading()
     let that = this
     let tempLat = '';
     wx.request({
@@ -74,15 +74,15 @@ Page({
       success: function(res) {
         if (res.data.code == 200) {
           that.hideNavigationBarLoading()
-            that.closeLoading()
-            console.log(res.data.data)
+          that.closeLoading()
+          console.log(res.data.data)
           tempLat = res.data.data.latlng.split(",")
           let lo = tempLat[1]
           let la = tempLat[0]
           that.setData({
             detail: res.data.data,
             overview: res.data.data.overview,
-              landscape: res.data.data.landscape.replace(/\s*/g,""),
+            landscape: res.data.data.landscape.replace(/\s*/g, ""),
             longitude: lo,
             latitude: la,
             markers: [{
@@ -131,47 +131,47 @@ Page({
           that.setData({
             imgUrls: pics
           })
-            //此处想灵活显示折叠按钮,但是由于接口请求有网络延时,用以下获取高度的方法效果不好
-            // if (that.data.viewHightBigStatus == 0) {
-            //     //通过定时0.3秒后检查楼盘介绍的文本折叠框 是否需要折叠按钮 ,此处运用到多个元素进行辅助比较
-            //     setTimeout(() => {
-            //         console.log(that.data.viewHightBigStatus+'aaaaaaaaaaaaa')
-            //     //创建节点选择器
-            //         let query = wx.createSelectorQuery()
-            //         //选择id
-            //         query.select('#viewHightOnlyCCC').boundingClientRect()
-            //     query.exec(function(res) {
-            //         //res就是 所有标签为mjltest的元素的信息 的数组
-            //         // console.log(res);
-            //         //取高度
-            //         // console.log(res[0].height);
-            //         let big = res[0].height
-            //         //创建节点选择器
-            //         let query1 = wx.createSelectorQuery()
-            //         //获取折叠文本的高度
-            //         query1.select('#viewHightOnly').boundingClientRect()
-            //         query1.exec(function(res1) {
-            //             if (res1[0].height) {
-            //                 //取高度 判断是否需要 折叠按钮
-            //                 if (big <= res1[0].height) {
-            //                     that.setData({
-            //                         viewHightImg: true
-            //                     })
-            //                 } else {
-            //                     that.setData({
-            //                         viewHightImg: false
-            //                     })
-            //                 }
-            //                 //隐藏辅助高度比较元素
-            //                 that.setData({
-            //                     viewHightBig: true,
-            //                     viewHightBigStatus:1
-            //                 })
-            //             }
-            //         })
-            //     })
-            // }, 1500)
-            // }
+          //此处想灵活显示折叠按钮,但是由于接口请求有网络延时,用以下获取高度的方法效果不好
+          // if (that.data.viewHightBigStatus == 0) {
+          //     //通过定时0.3秒后检查楼盘介绍的文本折叠框 是否需要折叠按钮 ,此处运用到多个元素进行辅助比较
+          //     setTimeout(() => {
+          //         console.log(that.data.viewHightBigStatus+'aaaaaaaaaaaaa')
+          //     //创建节点选择器
+          //         let query = wx.createSelectorQuery()
+          //         //选择id
+          //         query.select('#viewHightOnlyCCC').boundingClientRect()
+          //     query.exec(function(res) {
+          //         //res就是 所有标签为mjltest的元素的信息 的数组
+          //         // console.log(res);
+          //         //取高度
+          //         // console.log(res[0].height);
+          //         let big = res[0].height
+          //         //创建节点选择器
+          //         let query1 = wx.createSelectorQuery()
+          //         //获取折叠文本的高度
+          //         query1.select('#viewHightOnly').boundingClientRect()
+          //         query1.exec(function(res1) {
+          //             if (res1[0].height) {
+          //                 //取高度 判断是否需要 折叠按钮
+          //                 if (big <= res1[0].height) {
+          //                     that.setData({
+          //                         viewHightImg: true
+          //                     })
+          //                 } else {
+          //                     that.setData({
+          //                         viewHightImg: false
+          //                     })
+          //                 }
+          //                 //隐藏辅助高度比较元素
+          //                 that.setData({
+          //                     viewHightBig: true,
+          //                     viewHightBigStatus:1
+          //                 })
+          //             }
+          //         })
+          //     })
+          // }, 1500)
+          // }
 
         }
       },
@@ -186,48 +186,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
-      let that = this
-      //由于网络请求有延时,这个方法可行,但是不好
-      // if (that.data.viewHightBigStatus == 0) {
-      //     //通过定时0.3秒后检查楼盘介绍的文本折叠框 是否需要折叠按钮 ,此处运用到多个元素进行辅助比较
-      //     setTimeout(() => {
-      //         console.log(that.data.viewHightBigStatus+"ssssssssssssss")
-      //     //创建节点选择器
-      //     let query = wx.createSelectorQuery()
-      //     //选择id
-      //     query.select('#viewHightOnlyCCC').boundingClientRect()
-      //     query.exec(function(res) {
-      //         //res就是 所有标签为mjltest的元素的信息 的数组
-      //         // console.log(res);
-      //         //取高度
-      //         // console.log(res[0].height);
-      //         let big = res[0].height
-      //         //创建节点选择器
-      //         let query1 = wx.createSelectorQuery()
-      //         //获取折叠文本的高度
-      //         query1.select('#viewHightOnly').boundingClientRect()
-      //         query1.exec(function(res1) {
-      //             if (res1[0].height) {
-      //                 //取高度 判断是否需要 折叠按钮
-      //                 if (big <= res1[0].height) {
-      //                     that.setData({
-      //                         viewHightImg: true
-      //                     })
-      //                 } else {
-      //                     that.setData({
-      //                         viewHightImg: false
-      //                     })
-      //                 }
-      //                 //隐藏辅助高度比较元素
-      //                 that.setData({
-      //                     viewHightBig: true,
-      //                     viewHightBigStatus:1
-      //                 })
-      //             }
-      //         })
-      //     })
-      // }, 1300)
-      // }
+    let that = this
+    //由于网络请求有延时,这个方法可行,但是不好
+    // if (that.data.viewHightBigStatus == 0) {
+    //     //通过定时0.3秒后检查楼盘介绍的文本折叠框 是否需要折叠按钮 ,此处运用到多个元素进行辅助比较
+    //     setTimeout(() => {
+    //         console.log(that.data.viewHightBigStatus+"ssssssssssssss")
+    //     //创建节点选择器
+    //     let query = wx.createSelectorQuery()
+    //     //选择id
+    //     query.select('#viewHightOnlyCCC').boundingClientRect()
+    //     query.exec(function(res) {
+    //         //res就是 所有标签为mjltest的元素的信息 的数组
+    //         // console.log(res);
+    //         //取高度
+    //         // console.log(res[0].height);
+    //         let big = res[0].height
+    //         //创建节点选择器
+    //         let query1 = wx.createSelectorQuery()
+    //         //获取折叠文本的高度
+    //         query1.select('#viewHightOnly').boundingClientRect()
+    //         query1.exec(function(res1) {
+    //             if (res1[0].height) {
+    //                 //取高度 判断是否需要 折叠按钮
+    //                 if (big <= res1[0].height) {
+    //                     that.setData({
+    //                         viewHightImg: true
+    //                     })
+    //                 } else {
+    //                     that.setData({
+    //                         viewHightImg: false
+    //                     })
+    //                 }
+    //                 //隐藏辅助高度比较元素
+    //                 that.setData({
+    //                     viewHightBig: true,
+    //                     viewHightBigStatus:1
+    //                 })
+    //             }
+    //         })
+    //     })
+    // }, 1300)
+    // }
 
   },
 
@@ -288,30 +288,30 @@ Page({
       isFold: !this.data.isFold,
     })
   },
-    /**
-     * 楼盘参数文本折叠
-     */
-    showAllisFoldParam: function(e) {
-        this.setData({
-            isFoldParam: !this.data.isFoldParam,
-        })
-    },
-    //页面显示加载动画
-    showNavigationBarLoading: function () {
-        wx.showNavigationBarLoading()
-    },
-    //页面隐藏加载动画
-    hideNavigationBarLoading: function () {
-        wx.hideNavigationBarLoading()
-    },
-    openLoading: function () {
-        wx.showToast({
-            title: '数据加载中',
-            icon: 'loading',
-            duration: 3000
-        });
-    },
-    closeLoading: function () {
-        wx.hideToast()
-    }
+  /**
+   * 楼盘参数文本折叠
+   */
+  showAllisFoldParam: function(e) {
+    this.setData({
+      isFoldParam: !this.data.isFoldParam,
+    })
+  },
+  //页面显示加载动画
+  showNavigationBarLoading: function() {
+    wx.showNavigationBarLoading()
+  },
+  //页面隐藏加载动画
+  hideNavigationBarLoading: function() {
+    wx.hideNavigationBarLoading()
+  },
+  openLoading: function() {
+    wx.showToast({
+      title: '数据加载中',
+      icon: 'loading',
+      duration: 3000
+    });
+  },
+  closeLoading: function() {
+    wx.hideToast()
+  }
 })
